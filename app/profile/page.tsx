b3552aa872db755efc0b3f9fce5825c9a69a66c0
Damian Adamus <adamus.damian@yahoo.com>
2025-11-09 20:16:24 +0100

    Adding state to ProfilePage

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import {useEffect, useState} from "react";
+
 export interface UserProfile {
     id: string;
     full_name: string;
@@ -29,6 +31,31 @@ export interface UserPreferences {
 }
 
 export default function ProfilePage() {
+    const [profile, setProfile] = useState<UserProfile | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function loadProfile() {
+            try {
+                const profileData = await getCurrentUserProfile();
+                console.log(profileData);
+                if (profileData) {
+                    setProfile(profileData);
+                } else {
+                    setError("Failed to load profile");
+                }
+            } catch (err) {
+                console.error("Error loading profile: ", err);
+                setError("Failed to load profile");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        loadProfile();
+    }, []);
+
     return (
         <div>
             <div></div>
